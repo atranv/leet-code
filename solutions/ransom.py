d2d5677b01f97f3a29bfa85a00b1f74879d11b95
Alex Tran <alextranvt@gmail.com>
2024-02-29 12:30:02 -0500

    leetcode 383 solution

---
+++ solutions/ransom.py
@@ -0,0 +1,9 @@
+
+def canConstruct(ransomNote, magazine):
+    ransom_letters = list(ransomNote)
+    magazine_letters = list(magazine)
+
+    print(ransom_letters)
+    print(magazine_letters)  
+        
+print(canConstruct("ab", "aab"))
